7c70c644e74af449160bdf26f97444ec811e16a0
ApoorvaJu08 <apoorva10339@gmail.com>
2020-04-02 23:01:43 +0530

    added get client method

--- clientPanel/src/app/services/client.service.ts
+++ clientPanel/src/app/services/client.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } 
+from '@angular/fire/firestore';
+import { Client } from './../models/Client';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
-
-  constructor() { }
+  clientsCollection: AngularFirestoreCollection<Client>;
+  clientDoc: AngularFirestoreDocument<Client>;
+  clients: Observable<Client[]>
+  client: Observable<Client>;
+  constructor(private afs: AngularFirestore) { 
+    this.clientsCollection = this.afs.collection('clients', ref => ref.orderBy('lastName', 'asc'));
+  }
+  getClients(): Observable<Client[]> {
+    this.clients = this.clientsCollection.snapshotChanges().pipe(
+      map(changes => {
+        return changes.map(action => {
+          const data = action.payload.doc.data() as Client;
+          data.id = action.payload.doc.id;
+          return data;
+        });
+      })
+    )
+      return this.clients;
+  }
 }
